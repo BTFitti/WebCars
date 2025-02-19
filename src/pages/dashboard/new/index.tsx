3edd146e42e4a0e846a3068e24b16fb01a7c6e78
BTFitti <bruno.fitti734@live.com>
2025-02-19 18:50:47 -0300

    fix: fixed the firebase connection with the storage

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -2,89 +2,89 @@ import { ChangeEvent, useState, useContext } from "react";
 import { Container } from "../../../components/container";
 import { DashHeader } from "../../../components/dashboardHeader";
 
-import { FiUpload } from 'react-icons/fi'
-import { useForm } from 'react-hook-form'
-import Input from '../../../components/input'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { FiUpload } from "react-icons/fi";
+import { useForm } from "react-hook-form";
+import Input from "../../../components/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { AuthContext } from "../../../context/authContext";
-import { v4 as uuidV4 } from 'uuid'
+import { v4 as uuidV4 } from "uuid";
 
 import { storage } from "../../../services/firebaseConnections";
 import {
   ref,
   uploadBytes,
   getDownloadURL,
-  deleteObject
-} from 'firebase/storage'
+  deleteObject,
+} from "firebase/storage";
 
 const schema = z.object({
   name: z.string().nonempty("O campo nome é obrigatório"),
   model: z.string().nonempty("O modelo é obrigatório"),
   year: z.string().nonempty("O Ano do carro é obrigatório"),
-  km: z.string().nonempty("O KM do carro é obrigatório"),
+  km: z.string().nonempty("A quilometragem do carro é obrigatória"),
   price: z.string().nonempty("O preço é obrigatório"),
   city: z.string().nonempty("A cidade é obrigatória"),
-  whatsapp: z.string().min(1, "O Telefone é obrigatório").refine((value) => /^(\d{11,12})$/.test(value), {
-    message: "Numero de telefone invalido."
-  }),
-  description: z.string().nonempty("A descrição é obrigatória")
-})
+  whatsapp: z
+    .string()
+    .min(1, "O Telefone é obrigatório")
+    .refine((value) => /^(\d{11,12})$/.test(value), {
+      message: "Numero de telefone inválido.",
+    }),
+  description: z.string().nonempty("A descrição é obrigatória"),
+});
 
 type FormData = z.infer<typeof schema>;
 
-
 export function New() {
   const { user } = useContext(AuthContext);
-  const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
     resolver: zodResolver(schema),
-    mode: "onChange"
-  })
+    mode: "onChange",
+  });
 
+  async function handleFile(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files[0]) {
+      const image = e.target.files[0];
 
-  async function handleFile(e: ChangeEvent<HTMLInputElement>){
-    if(e.target.files && e.target.files[0]){
-      const image = e.target.files[0]
-
-      if(image.type === 'image/jpeg' || image.type === 'image/png'){
-        await handleUpload(image)
-      }else{
-        alert("Envie uma imagem jpeg ou png!")
+      if (image.type === "image/jpeg" || image.type === "image/png") {
+        await handleUpload(image);
+      } else {
+        alert("Envie uma imagem jpeg ou png!");
         return;
       }
-
-
     }
   }
-  
-  
-  async function handleUpload(image: File){
-    if(!user?.uid){
+
+  async function handleUpload(image: File) {
+    if (!user?.uid) {
       return;
     }
 
     const currentUid = user?.uid;
     const uidImage = uuidV4();
 
-    const uploadRef = ref(storage, `images/${currentUid}/${uidImage}`)
+    const uploadRef = ref(storage, `images/${currentUid}/${uidImage}`);
 
-    uploadBytes(uploadRef, image)
-    .then((snapshot) => {
-        getDownloadURL(snapshot.ref).then((downloadUrl) => {
-          console.log("URL DE ACESSO DA FOTO ", downloadUrl);
-        })
-    })
-
+    uploadBytes(uploadRef, image).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((downloadUrl) => {
+        console.log("URL DE ACESSO DA FOTO ", downloadUrl);
+      });
+    });
   }
 
-  function onSubmit(data: FormData){
+  function onSubmit(data: FormData) {
     console.log(data);
   }
 
-
   return (
     <Container>
-      <DashHeader/>
+      <DashHeader />
 
       <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
         <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
@@ -92,21 +92,18 @@ export function New() {
             <FiUpload size={30} color="#000" />
           </div>
           <div className="cursor-pointer">
-            <input 
-              type="file" 
-              accept="image/*" 
-              className="opacity-0 cursor-pointer" 
-              onChange={handleFile} 
+            <input
+              type="file"
+              accept="image/*"
+              className="opacity-0  h-32 w-full cursor-pointer"
+              onChange={handleFile}
             />
           </div>
         </button>
       </div>
 
       <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
-        <form
-          className="w-full"
-          onSubmit={handleSubmit(onSubmit)}  
-        >
+        <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-3">
             <p className="mb-2 font-medium">Nome do carro</p>
             <Input
@@ -130,18 +127,18 @@ export function New() {
           </div>
 
           <div className="flex w-full mb-3 flex-row items-center gap-4">
-            <div className="w-full">
+            <div>
               <p className="mb-2 font-medium">Ano</p>
               <Input
                 type="text"
                 register={register}
                 name="year"
                 error={errors.year?.message}
-                placeholder="Ex: 2016/2016..."
+                placeholder="Ex: 2016/2017..."
               />
             </div>
 
-            <div className="w-full">
+            <div>
               <p className="mb-2 font-medium">KM rodados</p>
               <Input
                 type="text"
@@ -151,19 +148,17 @@ export function New() {
                 placeholder="Ex: 23.900..."
               />
             </div>
-
           </div>
-
 
           <div className="flex w-full mb-3 flex-row items-center gap-4">
             <div className="w-full">
-              <p className="mb-2 font-medium">Telefone / Whatsapp</p>
+              <p className="mb-2 font-medium"> Whatsapp</p>
               <Input
                 type="text"
                 register={register}
                 name="whatsapp"
                 error={errors.whatsapp?.message}
-                placeholder="Ex: 011999101923..."
+                placeholder="Ex: 011945507281..."
               />
             </div>
 
@@ -174,10 +169,9 @@ export function New() {
                 register={register}
                 name="city"
                 error={errors.city?.message}
-                placeholder="Ex: Campo Grande - MS..."
+                placeholder="Ex: São Caetano do Sul - SP..."
               />
             </div>
-
           </div>
 
           <div className="mb-3">
@@ -187,28 +181,32 @@ export function New() {
               register={register}
               name="price"
               error={errors.price?.message}
-              placeholder="Ex: 69.000..."
+              placeholder="Ex: 84.000..."
             />
           </div>
 
           <div className="mb-3">
             <p className="mb-2 font-medium">Descrição</p>
             <textarea
-              className="border-2 w-full rounded-md h-24 px-2"
+              className="border-2 border-gray-400 w-full rounded-md h-24 px-2"
               {...register("description")}
               name="description"
               id="description"
               placeholder="Digite a descrição completa sobre o carro..."
             />
-            {errors.description && <p className="mb-1 text-red-500">{errors.description.message}</p>}
+            {errors.description && (
+              <p className="mb-1 text-red-500">{errors.description.message}</p>
+            )}
           </div>
 
-          <button type="submit" className="w-full rounded-md bg-zinc-900 text-white font-medium h-10">
+          <button
+            type="submit"
+            className="w-full cursor-pointer rounded-md bg-zinc-900 text-white font-medium h-10"
+          >
             Cadastrar
           </button>
-
         </form>
       </div>
     </Container>
-  )
+  );
 }
--- src/services/firebaseConnections.ts
+++ src/services/firebaseConnections.ts
@@ -23,28 +23,3 @@ const storage = getStorage(app)
 
 //exportando para acessar em qualquer parte do arquivo
 export {db, auth, storage};
-
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBr1NE4ysN-SreuJNLE9_GR5S_TA0mrEyU",
-//   authDomain: "webcarros2-89d15.firebaseapp.com",
-//   projectId: "webcarros2-89d15",
-//   storageBucket: "webcarros2-89d15.firebasestorage.app",
-//   messagingSenderId: "1086559006855",
-//   appId: "1:1086559006855:web:e939153af86ca526d40822"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-// const auth = getAuth(app);
-// const storage = getStorage(app)
-// export {db, storage, auth}
