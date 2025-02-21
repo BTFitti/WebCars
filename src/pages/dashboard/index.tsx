9a9ee199262c1cb3e915620cb15203a973f0508b
BTFitti <bruno.fitti734@live.com>
2025-02-21 17:00:17 -0300

    feat: rendering the car that a user creates

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,10 +2,17 @@ import { Container } from "../../components/container";
 import { DashHeader } from "../../components/dashboardHeader";
 import { FiTrash2 } from "react-icons/fi";
 import { useContext, useEffect, useState } from "react";
-import { collection, getDocs, where, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  where,
+  query,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import { db } from "../../services/firebaseConnections";
 import { AuthContext } from "../../context/authContext";
-interface CarProps{
+interface CarProps {
   id: string;
   name: string;
   year: string;
@@ -16,23 +23,22 @@ interface CarProps{
   uid: string;
 }
 
-interface ImageCarProps{
+interface ImageCarProps {
   name: string;
   uid: string;
-  url: string
+  url: string;
 }
 export function Dashboard() {
-
-  const {user} = useContext(AuthContext)
-  const [cars, setCars] = useState<CarProps[]>([])
+  const { user } = useContext(AuthContext);
+  const [cars, setCars] = useState<CarProps[]>([]);
 
   useEffect(() => {
     async function loadCars() {
-      if(!user?.uid){
+      if (!user?.uid) {
         return;
       }
       const carsRef = collection(db, "cars");
-      const queryRef = query(carsRef,where("uid", "==", user.uid) );
+      const queryRef = query(carsRef, where("uid", "==", user.uid));
       getDocs(queryRef)
         //snapshot é sempre oque encontrou
         .then((snapshot) => {
@@ -54,23 +60,46 @@ export function Dashboard() {
     }
     loadCars();
   }, [user]);
+
+  async function handleDeleteCar(id: string) {
+    const docRef = doc(db, "cars", id);
+    await deleteDoc(docRef);
+    setCars(cars.filter((car) => car.id !== id));
+  }
   return (
     <Container>
       <DashHeader />
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg relative">
-          <button onClick={()=>{}} className="absolute bg-white w-14 h-14 top-2 flex items-center justify-center right-4 rounded-full drop-shadow-2xl cursor-pointer">
-            <FiTrash2 size={32} color="#000"/>
-          </button>
-          <img className="w-full rounded-lg mb-2 max-h-70" src="https://firebasestorage.googleapis.com/v0/b/webcarros-f40c5.firebasestorage.app/o/images%2F2LphNyh70qOJfn9XyjeLc1m6HQQ2%2F4fdcb2a4-0943-4ea9-be30-9f5d29191d39?alt=media&token=71388623-66da-4b32-91c5-444ce80f8626" alt="" />
-          <p className="font-bold px-2 mt-1 mb-2">Nissan versa</p>
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700">Ano 2016/2019 | 23.090 km</span>
-            <strong className="text-black font-bold mt-4">R$ 150.000</strong>
-          </div>
-          <div className="w-full h-px bg-slate-200 my-2"></div>
-          <div className="px-2 pb-2"><span className="text-black">São caetano do Sul - SP</span></div>
-        </section>
+        {cars.map((car) => (
+          <section key={car.id} className="w-full bg-white rounded-lg relative">
+            <button
+              onClick={() => {
+                handleDeleteCar(car.id);
+              }}
+              className="absolute bg-white w-14 h-14 top-2 flex items-center justify-center right-4 rounded-full drop-shadow-2xl cursor-pointer"
+            >
+              <FiTrash2 size={32} color="#000" />
+            </button>
+            <img
+              className="w-full rounded-lg mb-2 max-h-70"
+              src={car.images[0].url}
+              alt=""
+            />
+            <p className="font-bold px-2 mt-1 mb-2">{car.name}</p>
+            <div className="flex flex-col px-2">
+              <span className="text-zinc-700">
+                Ano {car.year} | {car.km} km
+              </span>
+              <strong className="text-black font-bold mt-4">
+                R$ {car.price}
+              </strong>
+            </div>
+            <div className="w-full h-px bg-slate-200 my-2"></div>
+            <div className="px-2 pb-2">
+              <span className="text-black">{car.city}</span>
+            </div>
+          </section>
+        ))}
       </main>
     </Container>
   );
