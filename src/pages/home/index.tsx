1704fe0074ff2d40011b65f092518ef9caf992d8
BTFitti <bruno.fitti734@live.com>
2025-02-21 14:56:51 -0300

    feat: showing the cars registered in the home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,52 @@
 import { Container } from "../../components/container";
-// import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { db } from "../../services/firebaseConnections";
+import { Link } from "react-router-dom";
+interface CarsProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 export function Home() {
+  const [car, setCars] = useState<CarsProps[]>([]);
+
+  useEffect(() => {
+    async function loadCars() {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("created", "desc"));
+      getDocs(queryRef)
+        //snapshot é sempre oque encontrou
+        .then((snapshot) => {
+          let listCars = [] as CarsProps[];
+          snapshot.forEach((doc) => {
+            listCars.push({
+              id: doc.id,
+              name: doc.data().name,
+              year: doc.data().year,
+              km: doc.data().km,
+              city: doc.data().city,
+              price: doc.data().price,
+              images: doc.data().images,
+              uid: doc.data().uid,
+            });
+          });
+          setCars(listCars);
+        });
+    }
+    loadCars();
+  }, []);
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-2xl w-full max-w-5xl mx-auto flex justify-center items-center gap-1.5 lg:gap-3 mt-20">
@@ -15,35 +61,41 @@ export function Home() {
       <h1 className="font-bold text-center text-3xl my-10">
         Carros novos e usados em todo o Brasil!
       </h1>
+
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg pb-4 drop-shadow-2xl hover:border-b-8 border-b-red-700 transition-all duration-75 ease-in-out ">
-          <img
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2025/202501/20250124/toyota-yaris-1.5-16v-flex-xl-plus-connect-multidrive-wmimagem11135661047.jpg?s=fill&w=552&h=414&q=60"
-            alt="Veículo 1"
-            className="w-full rounded-t-lg max-h-72"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">Toyota Yaris</p>
-          <div className="flex flex-col px-2">
-            <strong className="text-black font-medium text-xl">
-              R$ 190.000
-            </strong>
-            <div className="flex justify-between mt-2 ">
-              <span className="text-zinc-700/60 ">2016/2017</span>
-              <span className="text-zinc-700/60">• 23.000 km</span>
-            </div>
-          </div>
-          <hr className="text-zinc-400/90 my-2" />
-          <div className="flex ">
-            <img
-              src="https://www.webmotors.com.br/assets/img/icon/icon-location.svg"
-              alt="Location"
-              className="pl-2"
-            />
-            <span className="px-2 text-zinc-700/60">
-              São Caetano do Sul - SP
-            </span>
-          </div>
-        </section>
+        {car.map((car) => (
+          <Link key={car.id} to={`/details/${car.id}`}>
+            <section className="w-full bg-white rounded-lg pb-4 drop-shadow-2xl hover:border-b-8 border-b-red-700 transition-all duration-75 ease-in-out ">
+              <img
+                src={car.images[0].url}
+                alt="Veículo 1"
+                className="w-full rounded-t-lg max-h-72"
+              />
+              <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
+              <div className="flex flex-col px-2">
+                <strong className="text-black font-medium text-xl">
+                  {car.price.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </strong>
+                <div className="flex justify-between mt-2 ">
+                  <span className="text-zinc-700/60 ">{car.year}</span>
+                  <span className="text-zinc-700/60">• {car.km} km </span>
+                </div>
+              </div>
+              <hr className="text-zinc-400/90 my-2" />
+              <div className="flex ">
+                <img
+                  src="https://www.webmotors.com.br/assets/img/icon/icon-location.svg"
+                  alt="Location"
+                  className="pl-2"
+                />
+                <span className="px-2 text-zinc-700/60">{car.city}</span>
+              </div>
+            </section>
+          </Link>
+        ))}
       </main>
     </Container>
   );
